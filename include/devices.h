4acc42c402048b8aea692ca24b539fd59186ca16
renee <149212078+321renee123@users.noreply.github.com>
2025-02-22 10:57:55 -0500

    restore values

--- include/devices.h
+++ include/devices.h
@@ -268,12 +268,12 @@ inline lemlib::OdomSensors sensors(&vertical_tracking_wheel,
 inline lemlib::ControllerSettings lateral_controller(5, // proportional gain (kP)
                                                0, // integral gain (kI)
                                             37, // derivative gain (kD)
-                                               0, // anti windup
-                                               0, // small error range, in inches
-                                               0, // small error range timeout, in milliseconds
-                                               0, // large error range, in inches
-                                               0, // large error range timeout, in milliseconds
-                                               0 // maximum acceleration (slew)
+                                            3, // anti windup
+                                            1, // small error range, in inches
+                                            100, // small error range timeout, in milliseconds
+                                            3, // large error range, in inches
+                                            500, // large error range timeout, in milliseconds
+                                            20 // maximum acceleration (slew)
  );
 
 
@@ -282,11 +282,11 @@ inline lemlib::ControllerSettings lateral_controller(5, // proportional gain (kP
 inline lemlib::ControllerSettings angular_controller(0.7, // proportional gain (kP)
                                                0, // integral gain (kI)
                                                0.64, // derivative gain (kD)
-                                               0, // anti windup
-                                               0, // small error range, in degrees
-                                               0, // small error range timeout, in milliseconds
-                                               0, // large error range, in degrees
-                                               0, // large error range timeout, in milliseconds
+                                               3, // anti windup
+                                               1, // small error range, in degrees
+                                               100, // small error range timeout, in milliseconds
+                                               3, // large error range, in degrees
+                                               500, // large error range timeout, in milliseconds
                                                0 // maximum acceleration (slew)
 );
 
